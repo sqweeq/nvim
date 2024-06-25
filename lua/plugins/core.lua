7ff744dfd4d76682d977287a34d4591713b7893d
sqweeq <tsai8@hotmail.com>
2024-06-25 15:31:04 -0400

    try some new config for nvim tree preview

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -20,6 +20,23 @@ return {
           visible = true,
         },
       },
+      -- window = {
+      --   mappings = {
+      --     ["P"] = { "toggle_preview", config = { use_float = true } },
+      --   },
+      -- },
+      -- event_handlers = {
+      --   {
+      --     event = "after_render",
+      --     handler = function()
+      --       local state = require("neo-tree.sources.manager").get_state("filesystem")
+      --       if not require("neo-tree.sources.common.preview").is_active() then
+      --         state.config = { use_float = false } -- or whatever your config is
+      --         state.commands.toggle_preview(state)
+      --       end
+      --     end,
+      --   },
+      -- },
     },
   },
   {
