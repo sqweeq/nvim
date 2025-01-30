8bc9e3c651d43c6ce1e6b731ad46a19d8e6eaa00
sqweeq <tsai8@hotmail.com>
2025-01-29 20:11:12 -0800

    fix

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -59,57 +59,65 @@ return {
     },
   },
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        -- "stylua",
-        -- "ruff_lsp",
-        "ruff",
-        -- "lua_language_server",
-        -- "shfmt",
-        -- "shellcheck",
-        -- "shfmt",
-        -- "flake8",
-      },
-    },
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = {
         enabled = true,
       },
     },
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        ensure_installed = {
+          -- "stylua",
+          "ruff-lsp",
+          "ruff",
+          -- "lua_language_server",
+          -- "shfmt",
+          -- "shellcheck",
+          -- "shfmt",
+          -- "flake8",
+        },
+      },
+    },
   },
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   -- dependencies = {
-  --   --   "mason.nvim",
-  --   --   { "williamboman/mason-lspconfig.nvim", config = function() end },
-  --   -- },
-  --   opts = {
-  --     servers = {
-  --       ruff_lsp = {
-  --         mason = true,
-  --         -- enabled = true,
-  --       },
-  --       ruff = {
-  --         mason = true,
-  --         -- enabled = true,
-  --       },
-  --       lua_language_server = {
-  --         mason = true,
-  --         -- enabled = true,
-  --       },
-  --       shfmt = {
-  --         mason = true,
-  --         -- enabled = true,
-  --       },
-  --       stylua = {
-  --         mason = true,
-  --         -- enabled = true,
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    -- dependencies = {
+    --   "mason.nvim",
+    --   { "williamboman/mason-lspconfig.nvim", config = function() end },
+    -- },
+    opts = {
+      servers = {
+        -- pyright = {
+        --   mason = false,
+        --   enabled = false,
+        -- },
+        -- black = {
+        --   mason = false,
+        --   enabled = false,
+        -- },
+        -- ruff_lsp = {
+        --   mason = true,
+        --   enabled = true,
+        -- },
+        ruff = {
+          mason = true,
+          enabled = true,
+        },
+        -- lua_language_server = {
+        --   mason = true,
+        --   -- enabled = true,
+        -- },
+        -- shfmt = {
+        --   mason = true,
+        --   -- enabled = true,
+        -- },
+        -- stylua = {
+        --   mason = true,
+        --   -- enabled = true,
+        -- },
+      },
+    },
+  },
 }
