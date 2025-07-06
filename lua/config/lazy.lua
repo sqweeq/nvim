1288f1dca28277f778dee9c796bdc9077fec6b80
Brandon Tsai <brandon@Mac.ts.net lan>
2025-07-06 16:11:47 -0700

    fix

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.omnisharp" },
     { import = "plugins" },
   },
-  --
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
