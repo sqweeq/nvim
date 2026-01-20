0afdf3dccf697afdb11f1180e7bae3fc646182ab
sqweeq <tsai8@hotmail.com>
2026-01-19 19:07:12 -0800

    clean up keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,30 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- https://vi.stackexchange.com/questions/39149/how-to-stop-neovim-from-yanking-text-on-pasting-over-selection
+local function map(m, k, v)
+  vim.keymap.set(m, k, v, { silent = true })
+end
+
+map("x", "p", "P")
+
+-- set C-j as esc instead
 vim.keymap.set("i", "<C-j>", "<ESC>", { remap = true })
 vim.keymap.set("n", "<C-j>", "<ESC>", { remap = true })
 vim.keymap.set("v", "<C-j>", "<ESC>", { remap = true })
 
 -- lua/config/keymaps.lua
 -- Remove a specific keymap, e.g., leader key combo
+-- remove delete hjkl
 vim.keymap.set("o", "h", "<ESC>", { desc = "which_key_ignore" })
 vim.keymap.set("o", "j", "<ESC>", { desc = "which_key_ignore" })
 vim.keymap.set("o", "k", "<ESC>", { desc = "which_key_ignore" })
 vim.keymap.set("o", "l", "<ESC>", { desc = "which_key_ignore" })
+
+-- leader dashboard
+vim.keymap.set("n", "<leader>D", function()
+  vim.cmd("Dashboard")
+end, { desc = "Dashboard" })
+
+-- leader recent files
+vim.keymap.set("n", "<leader>r", '<cmd>lua LazyVim.pick("oldfiles")()<cr>', { desc = " Recent Files" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,13 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- https://vi.stackexchange.com/questions/39149/how-to-stop-neovim-from-yanking-text-on-pasting-over-selection
-local function map(m, k, v)
-  vim.keymap.set(m, k, v, { silent = true })
-end
-
-map("x", "p", "P")
-
 local opt = vim.opt
 opt.list = false
 opt.conceallevel = 0
@@ -21,11 +14,3 @@ vim.g.loaded_netrwPlugin = 1
 --- Disable the inlay hints (additional information about types and parameters names) because it clutters my view
 -- vim.g.lazyvim_no_inlay_hints = true
 vim.g.snacks_animate = false
-
--- leader dashboard
-vim.keymap.set("n", "<leader>D", function()
-  vim.cmd("Dashboard")
-end, { desc = "Dashboard" })
-
--- leader recent files
-vim.keymap.set("n", "<leader>r", '<cmd>lua LazyVim.pick("oldfiles")()<cr>', { desc = " Recent Files" })
